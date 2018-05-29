def955149d594781942c4a2c7e53fa40233b4509
Gabriel <g2p.code@gmail.com>
2018-05-29 15:53:21 +0200

    rand: fill a buffer inside process memory

--- src/abi/rand.rs
+++ src/abi/rand.rs
@@ -11,13 +11,21 @@ fn get_rdrand() -> Result<RdRand> {
 static mut RDRAND : Option<Result<RdRand>> = None;
 
 #[nebulet_abi]
-pub fn random_u32(_process: &ProcessRef) -> Result<u32> {
+pub fn random_fill(buffer_offset: u32, buffer_size: u32, process: &ProcessRef) -> Result<u32> {
     let rdrand;
     unsafe {
         rdrand = RDRAND.get_or_insert_with(|| get_rdrand());
     }
     match rdrand {
-        Ok(ref mut v) => Ok(v.next_u32()),
+        Ok(ref mut v) => {
+            let mut instance = process.instance().write();
+            let memory = &mut instance.memories[0];
+
+            let buffer = memory.carve_slice_mut(buffer_offset, buffer_size)
+                .ok_or(Error::INVALID_ARG)?;
+            v.fill_bytes(buffer);
+            Ok(0)
+        },
         // XXX Find a way to return the existing error
         _ => Err(Error::UNAVAILABLE),
     }
