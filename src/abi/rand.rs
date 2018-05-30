7128ab98b74785bb837b1c8908c4c175961cee48
Gabriel de Perthuis <g2p.code+github@gmail.com>
2018-05-30 18:48:01 +0200

    Add a way to get random numbers to the ABI (#35)
    * Expose rdrand
    
    Addresses #29.
    
    * Add a way to get random numbers to the ABI
    
    Closes #29.
    
    * Move the rand implementation to the device directory
    
    * rand: fill a buffer inside process memory
    
    * rand: Add a comment documenting the ABI

---
+++ src/abi/rand.rs
@@ -0,0 +1,37 @@
+use object::ProcessRef;
+use nabi::{Result, Error};
+use nebulet_derive::nebulet_abi;
+use arch::x64::devices::rand::RdRand;
+use rand_core::RngCore;
+
+fn get_rdrand() -> Result<RdRand> {
+    RdRand::new().ok_or(Error::UNAVAILABLE)
+}
+
+static mut RDRAND : Option<Result<RdRand>> = None;
+
+/// Provides random bytes
+/// No guarantee is made that the random bytes are of cryptographic
+/// quality, or that they were seeded from a good entropy pool.
+/// This currently requires the rdrand instruction, which is fast
+/// but not supported everywhere.
+#[nebulet_abi]
+pub fn random_fill(buffer_offset: u32, buffer_size: u32, process: &ProcessRef) -> Result<u32> {
+    let rdrand;
+    unsafe {
+        rdrand = RDRAND.get_or_insert_with(|| get_rdrand());
+    }
+    match rdrand {
+        Ok(ref mut v) => {
+            let mut instance = process.instance().write();
+            let memory = &mut instance.memories[0];
+
+            let buffer = memory.carve_slice_mut(buffer_offset, buffer_size)
+                .ok_or(Error::INVALID_ARG)?;
+            v.fill_bytes(buffer);
+            Ok(0)
+        },
+        // XXX Find a way to return the existing error
+        _ => Err(Error::UNAVAILABLE),
+    }
+}
--- src/arch/x64/devices/mod.rs
+++ src/arch/x64/devices/mod.rs
@@ -3,6 +3,7 @@ pub mod pic;
 pub mod rtc;
 pub mod pit;
 pub mod high_precision_timer;
+pub mod rand;
 
 pub unsafe fn init() {
     pic::init();
