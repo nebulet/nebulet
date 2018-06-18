325a9cdc682f4406196ee5562bc944ad5781f878
Lachlan Sneff <lachlan.sneff@gmail.com>
2018-06-18 16:39:08 -0400

    Scheduler bug fix

--- src/object/event.rs
+++ src/object/event.rs
@@ -4,20 +4,20 @@ use task::State;
 use nabi::{Result};
 
 #[derive(KernelRef)]
-pub struct EventRef {
+pub struct Event {
     thread: Ref<Thread>,
 }
 
-impl EventRef {
+impl Event {
     /// Create a new event.
     /// Sets the thread state
     /// to blocked.
-    pub fn new(thread: Ref<Thread>) -> Result<Ref<Self>> {
+    pub fn new(thread: Ref<Thread>) -> Event {
         thread.set_state(State::Blocked);
 
-        Ref::new(EventRef {
+        Event {
             thread,
-        })
+        }
     }
     
     /// Trigger the event.
